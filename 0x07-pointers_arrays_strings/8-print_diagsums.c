441768c58c59e070fc49d5c73160af30beb3f933
Dafluxtech <dayodeji31@yahoo.com>
2022-09-26 17:20:28 -0700

    Task 7 error fixed for function that prints the sum of two diagonals

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,17 +9,22 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i;
-	unsigned int sum, sum1;
+	int i = 0;
+	int j = size - 1;
+	int sum1 = 0;
+	int sum2 = 0;
 
-	sum = 0;
-	sum = 0;
-
-	for (i = 0; i < size; i++)
+	while (i <= (size * size))
 	{
-		sum += a[(size * i) + i];
-		sum1 += a[(size * (i + 1)) - (i + 1)];
+		sum1 = sum1 + a[i];
+		i = i + size + 1;
 	}
 
-	printf("%d, %d\n", sum, sum1);
+	while (j < (size * size - 1))
+	{
+		sum2 += a[j];
+		j = j + size - 1;
+	}
+
+	printf("%d, %d\n", sum1, sum2);
 }
