9af0cad97911847582c767f6380df2a719edb2cf
Dafluxtech <dayodeji31@yahoo.com>
2022-09-28 06:49:27 -0700

    Task 8 function that compares two strings.

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * wildcmp - compares two string if they are identical
+ * @s1: first string
+ * @s2: second string
+ * Return: returns 1 if the two strings are identical
+ * otherwise returns 0.
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (s1 == s2)
+		return (1);
+	else
+		return (0);
+}
