92d03434f0671c708e269e1adab4218669c40c54
Dafluxtech <dayodeji31@yahoo.com>
2022-10-06 08:48:26 -0700

    Header file updated

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,6 +3,7 @@
 int _putchar(char c);
 int main(void);
 void simple_print_buffer(char *buffer, unsigned int size);
+void simple_print_buffer(int *buffer, unsigned int size);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
