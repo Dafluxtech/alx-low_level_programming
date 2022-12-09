75261f30bd57b6f78b4cb6b4ad827c04ce8f625f
Dafluxtech <dayodeji31@yahoo.com>
2022-12-08 20:10:33 -0800

    task 2 add node and task 0 error fixed

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,28 +1,21 @@
 #include "lists.h"
 
 /**
- * print_dlistint -prints all the elements of a
- * dlistint_t list
- *
- * @h: head of the list
- * Return: the number of nodes
+ * print_dlistint - Prints all the elements of a dlistint_t list.
+ * @h: The head of the dlistint_t list.
+ * solocode
+ * Return: The number of nodes in the list.
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	int count;
+	size_t nodes = 0;
 
-	count = 0;
-
-	if (h == Null)
-		return (count);
-	while (h->prev != Null)
-		h = h->prev;
-	
-	while (h != Null)
+	while (h)
 	{
+		nodes++;
 		printf("%d\n", h->n);
-		count++;
 		h = h->next;
 	}
-	return (count);
+
+	return (nodes);
 }
